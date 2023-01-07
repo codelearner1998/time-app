b7e2ce280f1a463c6f8cb513df27d3a018258a29
MK1998 <86234913+codelearner1998@users.noreply.github.com>
2023-01-07 22:38:20 +0530

    removed extra folders or code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+
+  setInterval(handleClick , "1000")
+  
+
+  const now = new Date().toLocaleTimeString();
+
+  const [time, UpdatedTime] = useState(now);
+
+
+  function handleClick() {
+    const newTime = new Date().toLocaleTimeString();
+
+    UpdatedTime(newTime);
+
+   
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>{time}</h1>
+      <button onClick={handleClick}>Get Time</button>
     </div>
   );
 }
